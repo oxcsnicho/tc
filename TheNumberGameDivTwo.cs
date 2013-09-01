c4410c7ac9466524c41fb050294520f2aaea3d0e
yang <yang@sheepws.local>
2013-09-01 22:36:08 +0800

    TheNumberGameDivTwo: linear dp
    Generalization:
    * B wins iff n is odd or n is 2^k where k is odd
    * J wins otherwise

--- TheNumberGameDivTwo.cs
+++ TheNumberGameDivTwo.cs
@@ -7,90 +7,142 @@ using System.Text.RegularExpressions;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
-public class TheNumberGameDivTwo {
-    public string find(int n) {
-        string res;
-        return res;
+public class TheNumberGameDivTwo
+{
+    public string find(int n)
+    {
+        bool[] F = new bool[1001];
+        F[1] = false;
+        F[2] = false;
+        F[3] = false;
+        F[4] = true;
+        if (n >= 5)
+            for (int i = 5; i <= n; i++)
+            {
+                F[i] = false;
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                        if (F[i - j] == false)
+                        {
+                            F[i] = true;
+                            break;
+                        }
+                }
+            }
+        // BEGIN CUT HERE
+        for (int i = 0; i < F.Length; i++)
+        {
+            if (F[i]) print(i.ToString());
+        }
+        // END CUT HERE
+        return F[n] ? "John" : "Brus";
     }
 
-// BEGIN CUT HERE
+    // BEGIN CUT HERE
     static List<int> cases = new List<int> { };
-    public static void Test() {
-        try  {
-            eq(0,(new TheNumberGameDivTwo()).find(6),"John");
-            eq(1,(new TheNumberGameDivTwo()).find(2),"Brus");
-            eq(2,(new TheNumberGameDivTwo()).find(747),"Brus");
-            eq(3,(new TheNumberGameDivTwo()).find(128),"Brus");
-        } 
-        catch( Exception exx)  {
+    public static void Test()
+    {
+        try
+        {
+            eq(0, (new TheNumberGameDivTwo()).find(6), "John");
+            eq(1, (new TheNumberGameDivTwo()).find(2), "Brus");
+            eq(2, (new TheNumberGameDivTwo()).find(747), "Brus");
+            eq(3, (new TheNumberGameDivTwo()).find(128), "Brus");
+        }
+        catch (Exception exx)
+        {
             System.Console.WriteLine(exx);
-            System.Console.WriteLine( exx.StackTrace);
+            System.Console.WriteLine(exx.StackTrace);
         }
     }
-    private static void eq( int n, object have, object need) {
+    private static void eq(int n, object have, object need)
+    {
         if (cases != null && cases.Count > 0)
             if (!cases.Exists((a) => a == n))
                 return;
-        if( eq( have, need ) ) {
-            Debug.WriteLine( "Case "+n+" passed." );
-        } else {
-            Debug.Write( "Case "+n+" failed: expected " );
-            print( need );
-            Debug.Write( ", received " );
-            print( have );
+        if (eq(have, need))
+        {
+            Debug.WriteLine("Case " + n + " passed.");
+        }
+        else
+        {
+            Debug.Write("Case " + n + " failed: expected ");
+            print(need);
+            Debug.Write(", received ");
+            print(have);
             Debug.WriteLine("");
         }
     }
-    private static void eq( int n, Array have, Array need) {
+    private static void eq(int n, Array have, Array need)
+    {
         if (cases != null && cases.Count > 0)
             if (!cases.Exists((a) => a == n))
                 return;
-        if( have == null || have.Length != need.Length ) {
-            Debug.WriteLine("Case "+n+" failed: returned "+have.Length+" elements; expected "+need.Length+" elements.");
-            print( have );
-            print( need );
+        if (have == null || have.Length != need.Length)
+        {
+            Debug.WriteLine("Case " + n + " failed: returned " + have.Length + " elements; expected " + need.Length + " elements.");
+            print(have);
+            print(need);
             return;
         }
-        for( int i= 0; i < have.Length; i++ ) {
-            if( ! eq( have.GetValue(i), need.GetValue(i) ) ) {
-                Debug.WriteLine( "Case "+n+" failed. Expected and returned array differ in position "+i );
-                print( have );
-                print( need );
+        for (int i = 0; i < have.Length; i++)
+        {
+            if (!eq(have.GetValue(i), need.GetValue(i)))
+            {
+                Debug.WriteLine("Case " + n + " failed. Expected and returned array differ in position " + i);
+                print(have);
+                print(need);
                 return;
             }
         }
-        Debug.WriteLine("Case "+n+" passed.");
+        Debug.WriteLine("Case " + n + " passed.");
     }
-    private static bool eq( object a, object b ) {
-        if ( a is double && b is double ) {
-            return Math.Abs((double)a-(double)b) < 1E-9;
-        } else {
-            return a!=null && b!=null && a.Equals(b);
+    private static bool eq(object a, object b)
+    {
+        if (a is double && b is double)
+        {
+            return Math.Abs((double)a - (double)b) < 1E-9;
+        }
+        else
+        {
+            return a != null && b != null && a.Equals(b);
         }
     }
-    private static void print( object a ) {
-        if ( a is string ) {
+    private static void print(object a)
+    {
+        if (a is string)
+        {
             Debug.Write(string.Format("\"{0}\"", a));
-        } else if ( a is long ) {
+        }
+        else if (a is long)
+        {
             Debug.Write(string.Format("{0}L", a));
-        } else {
+        }
+        else
+        {
             Debug.Write(a);
         }
     }
-    private static void print( Array a ) {
-        if ( a == null) {
+    private static void print(Array a)
+    {
+        if (a == null)
+        {
             Debug.WriteLine("<NULL>");
         }
         Debug.Write('{');
-        for ( int i= 0; i < a.Length; i++ ) {
-            print( a.GetValue(i) );
-            if( i != a.Length-1 ) {
+        for (int i = 0; i < a.Length; i++)
+        {
+            print(a.GetValue(i));
+            if (i != a.Length - 1)
+            {
                 Debug.Write(", ");
             }
         }
-        Debug.WriteLine( '}' );
+        Debug.WriteLine('}');
     }
-// END CUT HERE
+    // END CUT HERE
 }
 
