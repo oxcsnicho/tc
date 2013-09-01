9218ee93ced9278faf25f34f053495e454950c55
yang <yang@sheepws.local>
2013-09-01 09:16:14 +0800

    GearsDiv2: linear DP, + special treatment at the end
    basing on this it can be improved to greedy

--- GearsDiv2.cs
+++ GearsDiv2.cs
@@ -1,40 +1,135 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
 using System;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 public class GearsDiv2
 {
-    public int getmin(string Directions)
+    public int getmin(string d)
     {
-        if (Directions.Length == 1)
-            return 0;
-        if (Directions[0] == Directions[Directions.Length - 1])
-            return Math.Min(getmin1(Directions.Substring(0, Directions.Length - 1)),
-                getmin1(Directions.Substring(1, Directions.Length - 1))) + 1;
-        return getmin1(Directions);
+        return Math.Min(getmin1(d + d[0]), getmin1(d.Substring(1, d.Length - 1)) + 1);
     }
-    public int getmin1(string Directions)
+    public int getmin1(string d)
     {
-        int[] F = new int[Directions.Length];
-        Array.Clear(F, 0, F.Length);
+        int[] F = new int[d.Length];
         F[0] = 0;
-        F[1] = Directions[0] == Directions[1] ? 1 : 0;
-        for (int i = 2; i < F.Length; i++)
+        F[1] = d[0] == d[1] ? 1 : 0;
+        for (int i = 2; i < d.Length; i++)
         {
-            if (Directions[i] != Directions[i - 1])
-                F[i] = F[i - 1];
-            else
-                F[i] = Math.Min(F[i - 1], F[i - 2]) + 1;
+            F[i] = (d[i] != d[i - 1] ? F[i - 1] : F[i - 2] + 1);
         }
-        return F[F.Length - 1];
+        return F[d.Length - 1];
     }
 
+    // BEGIN CUT HERE
+    static List<int> cases = new List<int> { };
+    public static void Test()
+    {
+        try
+        {
+            eq(0, (new GearsDiv2()).getmin("LRRR"), 1);
+            eq(1, (new GearsDiv2()).getmin("RRR"), 2);
+            eq(2, (new GearsDiv2()).getmin("LRLR"), 0);
+            eq(3, (new GearsDiv2()).getmin("LRLLRRLLLRRRLLLL"), 6);
+            eq(4, (new GearsDiv2()).getmin("RRRRRRRLRRRRRRRLRLRLLRLRLRLRRLRLRLLLRLRLLRLLRRLRRR"), 14);
+        }
+        catch (Exception exx)
+        {
+            System.Console.WriteLine(exx);
+            System.Console.WriteLine(exx.StackTrace);
+        }
+    }
+    private static void eq(int n, object have, object need)
+    {
+        if (cases != null && cases.Count > 0)
+            if (!cases.Exists((a) => a == n))
+                return;
+        if (eq(have, need))
+        {
+            Debug.WriteLine("Case " + n + " passed.");
+        }
+        else
+        {
+            Debug.Write("Case " + n + " failed: expected ");
+            print(need);
+            Debug.Write(", received ");
+            print(have);
+            Debug.WriteLine("");
+        }
+    }
+    private static void eq(int n, Array have, Array need)
+    {
+        if (cases != null && cases.Count > 0)
+            if (!cases.Exists((a) => a == n))
+                return;
+        if (have == null || have.Length != need.Length)
+        {
+            Debug.WriteLine("Case " + n + " failed: returned " + have.Length + " elements; expected " + need.Length + " elements.");
+            print(have);
+            print(need);
+            return;
+        }
+        for (int i = 0; i < have.Length; i++)
+        {
+            if (!eq(have.GetValue(i), need.GetValue(i)))
+            {
+                Debug.WriteLine("Case " + n + " failed. Expected and returned array differ in position " + i);
+                print(have);
+                print(need);
+                return;
+            }
+        }
+        Debug.WriteLine("Case " + n + " passed.");
+    }
+    private static bool eq(object a, object b)
+    {
+        if (a is double && b is double)
+        {
+            return Math.Abs((double)a - (double)b) < 1E-9;
+        }
+        else
+        {
+            return a != null && b != null && a.Equals(b);
+        }
+    }
+    private static void print(object a)
+    {
+        if (a is string)
+        {
+            Debug.Write(string.Format("\"{0}\"", a));
+        }
+        else if (a is long)
+        {
+            Debug.Write(string.Format("{0}L", a));
+        }
+        else
+        {
+            Debug.Write(a);
+        }
+    }
+    private static void print(Array a)
+    {
+        if (a == null)
+        {
+            Debug.WriteLine("<NULL>");
+        }
+        Debug.Write('{');
+        for (int i = 0; i < a.Length; i++)
+        {
+            print(a.GetValue(i));
+            if (i != a.Length - 1)
+            {
+                Debug.Write(", ");
+            }
+        }
+        Debug.WriteLine('}');
+    }
+    // END CUT HERE
 }
 
-
-
-// Powered by FileEdit
-// Powered by CodeProcessor
