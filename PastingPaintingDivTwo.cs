eb0565e7b2fefae9d0ff617b3d0987d607a4c78e
yang <yang@sheepws.local>
2013-09-02 23:32:20 +0800

    ignore bak

---
+++ PastingPaintingDivTwo.cs
@@ -0,0 +1,170 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+public class PastingPaintingDivTwo
+{
+    public long countColors(string[] clipboard, int T)
+    {
+        long res = 0;
+        map = clipboard;
+        for (int i = 0; i < map.Length; i++)
+        {
+            for (int j = 0; j < map[0].Length; j++)
+            {
+                res += find(i, j, T);
+            }
+        }
+        return res;
+    }
+
+    string[] map;
+    long find(int i, int j, int T)
+    {
+        if (map[i][j] != 'B')
+            return 0;
+        int k = 1;
+        while (i + k < map.Length && j + k < map[0].Length && map[i + k][j + k] != 'B') k++;
+        if (i + k == map.Length || j + k == map[0].Length)
+            return T;
+        return Math.Min(k, T);
+    }
+
+    // BEGIN CUT HERE
+    static List<int> cases = new List<int> { };
+    public static void Test()
+    {
+        try
+        {
+            eq(0, (new PastingPaintingDivTwo()).countColors(new string[] {
+               "..B",
+               "B..",
+               "BB."
+               }, 3), 10L);
+            eq(1, (new PastingPaintingDivTwo()).countColors(new string[] {
+               "B...",
+               "....",
+               "....",
+               "...B"
+               }, 2), 4L);
+            eq(2, (new PastingPaintingDivTwo()).countColors(new string[] { "BBB" }, 10000), 30000L);
+            eq(3, (new PastingPaintingDivTwo()).countColors(new string[] { "." }, 1000000000), 0L);
+            eq(4, (new PastingPaintingDivTwo()).countColors(new string[] {
+               "BB.",
+               ".B."
+               }, 100), 201L);
+            eq(5, (new PastingPaintingDivTwo()).countColors(new string[] {
+               "..........B..........",
+               ".........B.B.........",
+               "........B...B........",
+               ".......B.....B.......",
+               "......B..B.B..B......",
+               ".....B...B.B...B.....",
+               "....B...........B....",
+               "...B...B.....B...B...",
+               "..B.....BBBBBB....B..",
+               ".B..........BB.....B.",
+               "BBBBBBBBBBBBBBBBBBBBB"
+               }, 1000000000), 21000000071L);
+        }
+        catch (Exception exx)
+        {
+            System.Console.WriteLine(exx);
+            System.Console.WriteLine(exx.StackTrace);
+        }
+    }
+    private static void eq(int n, object have, object need)
+    {
+        if (cases != null && cases.Count > 0)
+            if (!cases.Exists((a) => a == n))
+                return;
+        if (eq(have, need))
+        {
+            Debug.WriteLine("Case " + n + " passed.");
+        }
+        else
+        {
+            Debug.Write("Case " + n + " failed: expected ");
+            print(need);
+            Debug.Write(", received ");
+            print(have);
+            Debug.WriteLine("");
+        }
+    }
+    private static void eq(int n, Array have, Array need)
+    {
+        if (cases != null && cases.Count > 0)
+            if (!cases.Exists((a) => a == n))
+                return;
+        if (have == null || have.Length != need.Length)
+        {
+            Debug.WriteLine("Case " + n + " failed: returned " + have.Length + " elements; expected " + need.Length + " elements.");
+            print(have);
+            print(need);
+            return;
+        }
+        for (int i = 0; i < have.Length; i++)
+        {
+            if (!eq(have.GetValue(i), need.GetValue(i)))
+            {
+                Debug.WriteLine("Case " + n + " failed. Expected and returned array differ in position " + i);
+                print(have);
+                print(need);
+                return;
+            }
+        }
+        Debug.WriteLine("Case " + n + " passed.");
+    }
+    private static bool eq(object a, object b)
+    {
+        if (a is double && b is double)
+        {
+            return Math.Abs((double)a - (double)b) < 1E-9;
+        }
+        else
+        {
+            return a != null && b != null && a.Equals(b);
+        }
+    }
+    private static void print(object a)
+    {
+        if (a is string)
+        {
+            Debug.Write(string.Format("\"{0}\"", a));
+        }
+        else if (a is long)
+        {
+            Debug.Write(string.Format("{0}L", a));
+        }
+        else
+        {
+            Debug.Write(a);
+        }
+    }
+    private static void print(Array a)
+    {
+        if (a == null)
+        {
+            Debug.WriteLine("<NULL>");
+        }
+        Debug.Write('{');
+        for (int i = 0; i < a.Length; i++)
+        {
+            print(a.GetValue(i));
+            if (i != a.Length - 1)
+            {
+                Debug.Write(", ");
+            }
+        }
+        Debug.WriteLine('}');
+    }
+    // END CUT HERE
+}
+
